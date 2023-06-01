29686c36bb20c71a7f8c1f94d57e1fa4181dcbda
maxime-vds <maxime.vandriessche@hotmail.be>
2023-06-01 15:59:59 +0200

    single product

--- client/src/components/Category/Category.jsx
+++ client/src/components/Category/Category.jsx
@@ -1,26 +1,43 @@
-// import { useParams } from "react-router-dom";
-// import useFetch from "../../hooks/useFetch";
-// import Products from "../Products/Products";
-// import "./Category.scss";
-const Category = () => {
-    // const { id } = useParams();
-    // const { data } = useFetch(
-    //     `/api/products?populate=*&[filters][categories][id]=${id}`
-    // );
-    return (
-        // <div className="category-main-content">
-        //     <div className="layout">
-        //         <div className="category-title">
-        //             {
-        //                 data?.data?.[0]?.attributes?.categories?.data?.[0]
-        //                     ?.attributes?.title
-        //             }
-        //         </div>
-        //         <Products innerPage={true} products={data} />
-        //     </div>
-        // </div>
-        <div>Category</div>
-    );
-};
+//  import { useParams } from "react-router-dom";
+//  import useFetch from "../../hooks/useFetch";
+//  import Products from "../Products/Products";
+//  import "./Category.scss";
+// const Category = () => {
+//      const { id } = useParams();
+//      const { data } = useFetch(
+//          `/api/products?populate=*&[filters][categories][id]=${id}`
+//      );
+//     return (
+//       <div className="category-main-content">
+//         <div className="layout">
+//           <div className="category-title">
+//             {
+//               data?.data?.[0]?.attributes?.categories?.data?.[0]?.attributes
+//                 ?.title
+//             }
+//           </div>
+//           <Products innerPage={true} products={data} />
+//         </div>
+//       </div>
+//     );
+// };
 
-export default Category;
+// export default Category;
+
+import "./Category.scss";
+import Products from "../Products/Products";
+
+const Category  = () => {
+    return ( 
+        <div className="category-main-content">
+            <div className="layout">
+                <div className="category-title">
+                    Category title 
+                </div>
+                <Products innerPage={true}/>
+            </div>
+        </div>
+     );
+}
+ 
+export default Category ;
