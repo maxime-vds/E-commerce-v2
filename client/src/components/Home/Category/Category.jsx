83cde50e898aa831a178848c1f70bb5dfeadc642
maxime-vds <maxime.vandriessche@hotmail.be>
2023-06-08 17:32:18 +0200

    api bug

--- client/src/components/Home/Category/Category.jsx
+++ client/src/components/Home/Category/Category.jsx
@@ -29,38 +29,24 @@
 // export default Category;
 
 import "./Category.scss";
-import cat1 from "../../../assets/cat1-1.jpg"
-import cat2 from "../../../assets/cat2_2.jpg"
-import cat3 from "../../../assets/cat3_3.jpg"
-import cat4 from "../../../assets/cat4_4.jpg"
-const Category = () => {
+// import cat1 from "../../../assets/cat1-1.jpg"
+// import cat2 from "../../../assets/cat2_2.jpg"
+// import cat3 from "../../../assets/cat3_3.jpg"
+// import cat4 from "../../../assets/cat4_4.jpg"
+const Category = ({ categories }) => {
 return (
   <div className="shop-by-category">
     <div className="categories">
-      <div className="category">
-        <div className="category-image">
-        <img src={cat1} alt="products" /> 
-        <p className="category-text">Headphones</p>
-        </div>       
-      </div>
-      <div className="category">
-        <div className="category-image">
-        <img src={cat2 } alt="products" /> 
-        <p className="category-text">Smart Watches</p>
-        </div>       
-      </div>
-      <div className="category">
-        <div className="category-image">
-        <img src={cat3} alt="products" /> 
-        <p className="category-text">Bluetooth Speakers</p>
-        </div>       
-      </div>
-      <div className="category">
-        <div className="category-image">
-        <img src={cat4} alt="products" /> 
-        <p className="category-text"> Wireless Earbuds</p>
-        </div>       
-      </div>
+      {categories?.data?.map((item)=> (
+         <div key={item.id} className="category">
+         <div className="category-image">
+         <img src={process.env.REACT_APP_DEV_URL + item.attributes.img.data.attributes.url} alt="products" /> 
+         <p className="category-text">Headphones</p>
+         </div>       
+       </div>
+      ) )}
+     
+      
       
     </div>
   </div>
