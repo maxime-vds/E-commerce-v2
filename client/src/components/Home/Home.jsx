e2eeb74ebed51d43ac7d5938cf80b2c4828f45e6
maxime-vds <maxime.vandriessche@hotmail.be>
2023-06-09 12:43:06 +0200

    stuck with the fetch api

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,58 +1,90 @@
-import React, { useEffect, useContext } from "react";
-import Banner from "./Banner/Banner";
+// import React, { useEffect, useContext } from "react";
+// import Banner from "./Banner/Banner";
+// import "./Home.scss";
+// import Category from "./Category/Category";
+// import Products from "../Products/Products";
+// import { fetchDataFromApi } from "../../utils/api";
+// import { Context } from "../../utils/context";
+
+// const Home = () => {
+//     const { products, setProducts, categories, setCategories } = useContext(Context);
+   
+//      useEffect(() => {
+//        getProducts();
+//        getCategories();
+//      },[]);
+
+//      const getProducts = () => {
+//          fetchDataFromApi("/api/products/id").then((res) => {
+//              setProducts(res);
+//          });
+//      };
+
+//     const getCategories = () => {
+//         fetchDataFromApi("/api/categories/id").then((res) => {
+//             console.log(res);
+//             setCategories(res);
+//         });
+//     };
+
+
+//     return (
+//         // <div>
+//         //     <Banner />
+//         //     <div className="main-content">
+//         //         <div className="layout">
+//         //             <Category categories={categories} />
+//         //             <Products
+//         //                 headingText="Popular Products"
+//         //                 products={products}
+//         //             />
+//         //         </div>
+//         //     </div>
+//         // </div>
+     
+//         <div className="home">
+//             <Banner/>
+//             <div className="main-content">
+//                 <div className="layout">
+//                 <Category categories={categories}/>
+//                 <Products products= {products} headingText="Popular Products"/>
+//                 </div>
+//             </div>
+//         </div>
+         
+//     );
+// };
+
+// export default Home;
+
 import "./Home.scss";
+import Banner from "./Banner/Banner";
 import Category from "./Category/Category";
 import Products from "../Products/Products";
-import { fetchDataFromApi } from "../../utils/api";
-import { Context } from "../../utils/context";
+import { useEffect } from "react";
+import { FetchDataFromApi } from "../../utils/api";
 
 const Home = () => {
-    const { products, setProducts, categories, setCategories } = useContext(Context);
-   
-     useEffect(() => {
-       getProducts();
-       getCategories();
-     },[]);
 
-     const getProducts = () => {
-         fetchDataFromApi("/api/products?populate=*").then((res) => {
-             setProducts(res);
-         });
+     useEffect (()=>{
+        getCategories ()
+     }, []);
+
+     const getCategories = () => {
+         FetchDataFromApi("/api/categories").then(res => console.log(res));
      };
 
-    const getCategories = () => {
-        fetchDataFromApi("/api/categories?populate=*").then((res) => {
-            console.log(res);
-            setCategories(res);
-        });
-    };
-
-
-    return (
-        // <div>
-        //     <Banner />
-        //     <div className="main-content">
-        //         <div className="layout">
-        //             <Category categories={categories} />
-        //             <Products
-        //                 headingText="Popular Products"
-        //                 products={products}
-        //             />
-        //         </div>
-        //     </div>
-        // </div>
-     
-        <div className="home">
+    return ( 
+        <div>
             <Banner/>
             <div className="main-content">
                 <div className="layout">
-                <Category categories={categories}/>
-                <Products products= {products} headingText="Popular Products"/>
+                    <Category/>
+                    <Products headingText="Popular Products"/>
                 </div>
             </div>
         </div>
-         
-    );
-};
-
+     );
+}
+ 
 export default Home;
--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,52 +1,76 @@
+// // import axios from "axios";
+
+// // const params = {
+// //     headers: {
+// //         Authorization: "bearer " + process.env.REACT_APP_STRIPE_DEV_APP_KEY,
+// //     },
+// // };
+
+// // export const fetchDataFromApi = async (url) => {
+// //     try {
+// //         const { data } = await axios.get(
+// //             process.env.REACT_APP_STRIPE_APP_DEV_URL + url,
+// //             params
+// //         );
+// //         return data;
+// //     } catch (err) {
+// //         console.log(err);
+// //         return err;
+// //     }
+// // };
+
+// // export const makePaymentRequest = axios.create({
+// //     baseURL: process.env.REACT_APP_STRIPE_APP_DEV_URL,
+// //     headers: {
+// //         Authorization: "bearer " + process.env.REACT_APP_STRIPE_DEV_APP_KEY,
+// //     },
+// // });
+
 // import axios from "axios";
 
-// const params = {
-//     headers: {
-//         Authorization: "bearer " + process.env.REACT_APP_STRIPE_DEV_APP_KEY,
-//     },
-// };
+// // const params = {
+// //   headers: {
+// //     Authorization: "bearer" + process.env.REACT_APP_STRIPE_APP_KEY,
+// //   },
+// // };
 
 // export const fetchDataFromApi = async (url) => {
-//     try {
-//         const { data } = await axios.get(
-//             process.env.REACT_APP_STRIPE_APP_DEV_URL + url,
-//             params
-//         );
-//         return data;
-//     } catch (err) {
-//         console.log(err);
-//         return err;
-//     }
-// };
-
-// export const makePaymentRequest = axios.create({
-//     baseURL: process.env.REACT_APP_STRIPE_APP_DEV_URL,
-//     headers: {
-//         Authorization: "bearer " + process.env.REACT_APP_STRIPE_DEV_APP_KEY,
-//     },
-// });
-
-import axios from "axios";
-
-// const params = {
-//   headers: {
-//     Authorization: "bearer" + process.env.REACT_APP_STRIPE_APP_KEY,
-//   },
-// };
-
-export const fetchDataFromApi = async (url) => {
   
-  try {
-    const data = await axios.get(
-      process.env.REACT_APP_DEV_URL + url,
-      {headers: {
-        Authorization: "bearer" + process.env.REACT_APP_STRIPE_APP_KEY,
-      },}
-    );
-    // return data;
-    console.log(data);
-  } catch (error) {
-    // console.log(error);
-    // return error;
-  }
+//   try {
+//     const data = await axios.get(
+//       process.env.REACT_APP_DEV_URL + url,
+//       {
+//         headers: {
+//         Authorization: "bearer" + process.env.REACT_APP_STRIPE_APP_KEY,
+//       },
+//     }
+//     );
+//     return data;
+//     // console.log(data);
+//   } catch (error) {
+//     console.log(error);
+//     // return error;
+//   }
+// };
+
+import axios from "axios"
+
+const params ={
+
+    headers: {
+      Authorization : "bearer" + process.env.REACT_APP_STRIPE_APP_KEY,
+    },
 };
+
+ export const FetchDataFromApi = async (url) => {
+   try {
+     const { data }= await axios.get(
+        process.env.REACT_APP_DEV_URL + url, 
+        params
+      );
+      return data;
+    } catch (error) {
+     console.log(error)
+     return error;
+   }
+ }
